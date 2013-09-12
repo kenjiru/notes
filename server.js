b38cbca2b56585673fd53538d1905a76ed26fce5
kenjiru <kenjiru.ro@gmail.com>
2013-09-12 23:48:09 +0200

    Changed the node.js http server to use express instead of connect.

--- server.js
+++ server.js
@@ -2,11 +2,32 @@
  * Small http static server.
  */
 
-var connect = require('connect'),
+var express = require('express'),
+    fs = require('fs'),
+    server  = express(),
+    root = './',
     port = process.env.PORT || 8080;
+
+server.get(root, function (req, res) {
+    console.log('serving index.html');
+    res.sendfile('index.html');
+});
+
+server.get('*', function (req, res) {
+    var requestedFile = req.url.substr(1);
+
+    if (fs.existsSync(requestedFile)) {
+        console.log('serving file: ' + requestedFile);
+        res.sendfile(requestedFile);
+    } else {
+        console.log('could not find file: ' + requestedFile);
+        res.sendfile('index.html');
+
+        // use this when serverRouting = true
+//        res.redirect(root + '#' + req.url.substr(4));
+    }
+});
 
 console.log("Listening on " + port);
 
-connect.createServer(
-    connect.static(__dirname)
-).listen(port);
+server.listen(port);
