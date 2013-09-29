67976cf8624ce3be21032ccba94fc42bb349234d
kenjiru <kenjiru.ro@gmail.com>
2013-09-29 23:31:41 +0200

    Configure the dependency injection .

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-YUI().use('notes-app', 'dropbox-demo', function(Y){
+YUI().use('notes-app', 'dropbox-proxy', function(Y) {
+    // configure the dependency injection
+    Y.di.configure(function(injector){
+        injector.bind('DropboxProxy').to(Y.notes.DropboxProxy).on(Y.di.Scope.SINGLETON);
+    });
+
     var app = new Y.notes.App();
     app.render().dispatch();
-
-    var dropboxDemo = new Y.notes.DropboxDemo();
 });
