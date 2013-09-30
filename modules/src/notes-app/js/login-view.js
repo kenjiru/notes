d21716a9c9d394632074b993a2c9263eb85bc9d2
kenjiru <kenjiru.ro@gmail.com>
2013-09-30 22:31:39 +0200

    Added different states (logged in/logged out) for the login view.

--- modules/src/notes-app/js/login-view.js
+++ modules/src/notes-app/js/login-view.js
@@ -1,12 +1,34 @@
 Y.namespace('notes').LoginView = Y.Base.create('loginView', Y.View, [], {
     _dropboxProxy : null,
-    _loginButton : null,
 
     initializer : function() {
         this._dropboxProxy = Y.di.inject('DropboxProxy');
     },
 
+    _checkAuthentication : function() {
+        this._dropboxProxy.isAuthenticated(function(ev){
+            if (ev.authenticated) {
+                this._uiShowLogoutButton();
+            } else {
+                this._uiShowLoginButton();
+            }
+        }, this);
+    },
+
     render : function() {
+        this._uiShowChecking();
+        this._checkAuthentication();
+
+        return this;
+    },
+
+    _uiShowChecking : function() {
+        var container = this.get('container');
+
+        container.set('text', 'Checking login status..');
+    },
+
+    _uiShowLoginButton : function() {
         var container = this.get('container'),
             loginButton = new Y.Button({
                 label : 'Login'
@@ -14,20 +36,25 @@ Y.namespace('notes').LoginView = Y.Base.create('loginView', Y.View, [], {
 
         container.set('text', 'You are not loggedin!');
         loginButton.render(container);
-
-        this._loginButton = loginButton;
-
-        this._bindUI();
-
-        return this;
+        loginButton.on('click', this._login, this);
     },
 
-    _bindUI : function() {
-        this._loginButton.on('click', this._login, this);
+    _uiShowLogoutButton : function() {
+        var container = this.get('container'),
+            loginButton = new Y.Button({
+                label : 'Login'
+            });
+
+        container.set('text', 'You are not loggedin!');
+        loginButton.render(container);
+        loginButton.on('click', this._logout, this);
     },
 
     _login : function() {
         console.log('login cliked!');
         this._dropboxProxy.authenticate();
+    },
+
+    _logout : function() {
     }
 });
