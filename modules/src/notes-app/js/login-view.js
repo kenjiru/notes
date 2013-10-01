b9abaa5dff92a3a9e185853b7210a859a03ec14b
kenjiru <kenjiru.ro@gmail.com>
2013-10-01 22:19:17 +0200

    Simplified the login view.

--- modules/src/notes-app/js/login-view.js
+++ modules/src/notes-app/js/login-view.js
@@ -11,20 +11,17 @@ Y.namespace('notes').LoginView = Y.Base.create('loginView', Y.View, [], {
     _checkAuthentication : function() {
         this._dropboxProxy.isAuthenticated(function(ev){
             if (ev.authenticated) {
-                this._uiShowLogout();
+                this._uiShowLoginSuccess();
             } else {
-                this._uiShowLoginError();
+                this._uiShowLogin();
             }
         }, this);
     },
 
     render : function() {
-        var template = "<div id='message'></div>" +
+        var template = "<div id='message'>Checking login status..</div>" +
             "<div id='login-panel' class='panel'>" +
             "   <input id='login-button' value='Login' type='button'/>" +
-            "</div>"  +
-            "<div id='logout-panel' class='panel'>" +
-            "   <input id='logout-button' value='Logout' type='button'/>" +
             "</div>",
             contentNode = Y.Node.create(template),
             container = this.get('container');
@@ -32,9 +29,7 @@ Y.namespace('notes').LoginView = Y.Base.create('loginView', Y.View, [], {
         this._ui = {
             message : contentNode.one('#message'),
             loginPanel : contentNode.one('#login-panel'),
-            logoutPanel : contentNode.one('#logout-panel'),
-            loginButton : contentNode.one('#login-button'),
-            logoutButton : contentNode.one('#logout-button')
+            loginButton : contentNode.one('#login-button')
         };
 
         contentNode.appendTo(container);
@@ -50,37 +45,25 @@ Y.namespace('notes').LoginView = Y.Base.create('loginView', Y.View, [], {
 
     _bindUi : function() {
         this._ui.loginButton.on('click', this._login, this);
-        this._ui.logoutButton.on('click', this._logout, this);
     },
 
     _uiShowChecking : function() {
         this._setMessage('Checking login status..');
+        this._ui.loginPanel.hide();
     },
 
     _uiShowLogin : function() {
         this._setMessage('You are not logged in!');
-        this._ui.logoutPanel.hide();
         this._ui.loginPanel.show();
     },
 
-    _uiShowLogout : function() {
-        this._setMessage('You are logged in!');
+    _uiShowLoginSuccess : function() {
+        this._setMessage('You\'ve been logged in successfully!');
         this._ui.loginPanel.hide();
-        this._ui.logoutPanel.show();
     },
 
     _uiShowLoginError : function() {
         this._setMessage('Error during authentication!');
-    },
-
-    _uiShowLogoutSuccess : function() {
-        this._setMessage('You\'ve been logged out successfully!');
-        this._ui.logoutPanel.hide();
-        this._ui.loginPanel.show();
-    },
-
-    _uiShowLogoutFail : function() {
-        this._setMessage('Failed to logout!');
     },
 
     _setMessage : function(msg) {
@@ -90,19 +73,9 @@ Y.namespace('notes').LoginView = Y.Base.create('loginView', Y.View, [], {
     _login : function() {
         this._dropboxProxy.authenticate(function(ev){
             if (ev.authenticated) {
-                this._uiShowLogout();
+                this._uiShowLoginSuccess();
             } else {
                 this._uiShowLoginError();
-            }
-        }, this);
-    },
-
-    _logout : function() {
-        this._dropboxProxy.signOut(function(ev){
-            if (ev.success) {
-                this._uiShowLogoutSuccess();
-            } else {
-                this._uiShowLogoutFail();
             }
         }, this);
     }
