3b06df8e0e2cd280c04d2e74b3617a7c948152b9
kenjiru <kenjiru.ro@gmail.com>
2013-10-14 23:19:07 +0200

    Mock the ManifestCache so we can test.

--- modules/src/notes-app/js/manifest-cache.js
+++ modules/src/notes-app/js/manifest-cache.js
@@ -6,11 +6,46 @@ var ManifestCache = Y.Base.create('manifestCache', Y.Base, [], {
      *  - notesMap - a map of notes, with the id of the note as the key
      *      * revision - note revision
      *      * title - note title
+     *      * last-change-date - the date when the note was last changed
      */
     _manifestObject : null,
 
     initializer : function(config) {
-        this._readFromCache();
+//        this._readFromCache();
+        this._mockCache();
+    },
+
+    _mockCache : function() {
+        this._manifestObject = {
+            revision : '3',
+            notesMap : {
+                '9f4cca5e-aaea-4268-9511-d27d496b1865' : {
+                    'revision' : '0',
+                    'title' : 'New Note Template',
+                    'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
+                },
+                'ae2be171-5fd2-4845-b6a5-5cd41b19fc67' : {
+                    'revision' : '1',
+                    'title' : 'Duis volutpat eget',
+                    'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
+                },
+                '82ea85e8-7f76-4071-ae73-2efe36ecef97' : {
+                    'revision' : '1',
+                    'title' : 'Lorem Ipsum',
+                    'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
+                },
+                '1794f7c1-96e7-474b-b41f-56e2da6f6e53' : {
+                    'revision' : '2',
+                    'title' : 'Phasellus volutpat',
+                    'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
+                },
+                '6a7bca2e-a436-42f8-bd67-4b0380490ad7' : {
+                    'revision' : '3',
+                    'title' : 'Morbi rutrum libero',
+                    'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
+                }
+            }
+        };
     },
 
     _readFromCache : function() {
@@ -21,10 +56,12 @@ var ManifestCache = Y.Base.create('manifestCache', Y.Base, [], {
         Y.StoreLite.setItem(this.STORAGE_KEY, this._manifestObject, true);
     },
 
-    addNote : function(note) {
+    addNoteInfo : function(note) {
         this._manifestObject.notesMap[note.id] = {
-            revision : note.revision,
-            title : note.title
+            'revision' : note['revision'],
+            'title' : note['title'],
+            'last-change-date' : note['last-change-date'],
+            'hit' : true
         };
     },
 
