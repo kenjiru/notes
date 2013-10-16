8c00848ea70e2fe1efd7754c8d88785f4e279326
kenjiru <kenjiru.ro@gmail.com>
2013-10-17 00:25:50 +0200

    Small refactoring.

--- modules/src/notes-app/js/manifest-file.js
+++ modules/src/notes-app/js/manifest-file.js
@@ -1,55 +1,46 @@
 var ManifestFile = Y.Base.create('manifestFile', Y.Base, [], {
     _dropboxProxy : null,
-    _callback : null,
 
     initializer : function(config) {
         this._dropboxProxy = Y.di.inject('DropboxProxy');
-        this._notesArray = [];
     },
 
     readFile : function(file, callback) {
-        this._callback = callback;
-        this._dropboxProxy.readFile(file, Y.bind(this._readManifestFile, this));
+        this._dropboxProxy.readFile(file, Y.bind(this._readManifestFile, this, callback));
     },
 
-    getNotes : function() {
-        return this._notesArray;
-    },
-
-    _readManifestFile : function(error, data) {
+    _readManifestFile : function(callback, error, data) {
         if (error) {
             console.log('could not read manifest file!');
             console.log(error);
-
-            this._callback.call(null, null, error);
             return;
         }
 
         console.log('Manifest file read successfully!');
-        this._parseXmlFile(data);
+        this._parseXmlFile(data, callback);
     },
 
-    _parseXmlFile : function(data) {
+    _parseXmlFile : function(data, callback) {
         var xmlDoc = Y.DataType.XML.parse(data),
             rootNode = xmlDoc.childNodes[0],
             revision = rootNode.getAttribute('revision'),
-            notesArray = [],
+            notesMeta = [],
             node;
 
         for(var i=0; i<rootNode.childNodes.length; i++) {
             node = rootNode.childNodes[i];
 
             if (node.nodeType == 1) {
-                notesArray.push({
+                notesMeta.push({
                     id : node.getAttribute('id'),
                     revision : node.getAttribute('rev')
                 })
             }
         }
 
-        this._callback.call(null, {
+        callback.call(null, {
             revision : revision,
-            notesArray : notesArray
+            notesMeta : notesMeta
         });
     }
 }, {
