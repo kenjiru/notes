18e76c81206cb36f67ab7ecbe5f54d0edd9de767
kenjiru <kenjiru.ro@gmail.com>
2013-10-17 00:25:22 +0200

    Added missing ids fro the mockup cache.

--- modules/src/notes-app/js/notes-info-cache.js
+++ modules/src/notes-app/js/notes-info-cache.js
@@ -20,26 +20,31 @@ var NotesInfoCache = Y.Base.create('notesInfoCache', Y.Base, [], {
             revision : '3',
             notesMap : {
                 '9f4cca5e-aaea-4268-9511-d27d496b1865' : {
+                    'id' : '9f4cca5e-aaea-4268-9511-d27d496b1865',
                     'revision' : '0',
                     'title' : 'New Note Template',
                     'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
                 },
                 'ae2be171-5fd2-4845-b6a5-5cd41b19fc67' : {
+                    'id' : 'ae2be171-5fd2-4845-b6a5-5cd41b19fc67',
                     'revision' : '1',
                     'title' : 'Duis volutpat eget',
                     'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
                 },
                 '82ea85e8-7f76-4071-ae73-2efe36ecef97' : {
+                    'id' : '82ea85e8-7f76-4071-ae73-2efe36ecef97',
                     'revision' : '1',
                     'title' : 'Lorem Ipsum',
                     'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
                 },
                 '1794f7c1-96e7-474b-b41f-56e2da6f6e53' : {
+                    'id' : '1794f7c1-96e7-474b-b41f-56e2da6f6e53',
                     'revision' : '2',
                     'title' : 'Phasellus volutpat',
                     'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
                 },
                 '6a7bca2e-a436-42f8-bd67-4b0380490ad7' : {
+                    'id' : '6a7bca2e-a436-42f8-bd67-4b0380490ad7',
                     'revision' : '3',
                     'title' : 'Morbi rutrum libero',
                     'last-change-date' : '2013-10-06T23:00:52.5663410+02:00'
