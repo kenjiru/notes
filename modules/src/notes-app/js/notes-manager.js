167303b0d5ed047a63428dcf75d14bdbae632a91
kenjiru <kenjiru.ro@gmail.com>
2013-10-18 21:28:32 +0200

    NotesManager doesn't process all the notes files when it should return only one note. Drop the events because they are redundant.

--- modules/src/notes-app/js/notes-manager.js
+++ modules/src/notes-app/js/notes-manager.js
@@ -7,6 +7,7 @@ var State = {
 
 var NotesManager = Y.Base.create('notesManager', Y.Base, [], {
     _dropboxProxy : null,
+    _manifestFile : null,
     _notesInfoList : null,
     _notesCache : null,
     _notesLength : null,
@@ -15,47 +16,60 @@ var NotesManager = Y.Base.create('notesManager', Y.Base, [], {
 
     initializer : function(config) {
         this._dropboxProxy = Y.di.inject('DropboxProxy');
+        this._manifestFile = new Y.notes.ManifestFile();
         this._notesInfoList = new Y.ModelList();
         this._notesCache = new Y.notes.NotesCache();
         this._notesProcessed = 0;
         this._internalState = State.UNKNOWN;
-
-        this.publish('noteRead');
-        this.publish('allNotesRead');
-        this.publish('error');
     },
 
-    readNotes : function() {
-        if (this._internalState !== State.LOADED) {
-            this._readManifestFile();
+    createNotesInfoList : function() {
+        this._readManifestFile(Y.bind(this._getAllNotesInfo, this));
+    },
+
+    getNote : function(id, callback) {
+        this._readManifestFile(Y.bind(this._getNote, this, id, callback));
+    },
+
+    _getNote : function(id, callback, manifest) {
+        var noteMeta = manifest.notesMeta[id];
+
+        this._notesCache.getNoteInfo(noteMeta, Y.bind(this._returnNote, this, callback));
+    },
+
+    _returnNote : function(callback, noteInfo) {
+        this._notesCache.getNote(noteInfo, callback);
+    },
+
+    _readManifestFile : function(callback) {
+        if (this._internalState == State.LOADED || this._internalState == State.LOADING) {
+            callback.call(null, this._manifestFile.getManifest());
+        } else {
+            this._manifestFile.readFile('/manifest.xml', Y.bind(this._onReadManifest, this, callback));
         }
     },
 
-    _readManifestFile : function() {
-        var manifestFile = new Y.notes.ManifestFile();
-
-        manifestFile.readFile('/manifest.xml', Y.bind(this._onReadManifest, this));
-    },
-
-    _onReadManifest : function(manifest, error) {
-        var notesMeta;
-
+    _onReadManifest : function(callback, manifest, error) {
         if (error) {
             this._internalState = State.ERROR;
-            this.fire('error');
-
             return;
         }
 
-        this._internalState = State.LOADING;
-
         this._notesCache.setRevision(manifest.revision);
 
-        notesMeta = manifest.notesMeta;
-        this._notesLength = notesMeta.length;
+        callback.call(null, manifest);
+    },
 
-        for(var i=0; i<notesMeta.length; i++) {
-            this._notesCache.getNoteInfo(notesMeta[i], Y.bind(this._addNoteInfo, this));
+    _getAllNotesInfo : function(manifest) {
+        var notesMeta = manifest.notesMeta;
+
+        this._internalState = State.LOADING;
+        this._notesLength = manifest.notesLength;
+
+        for (var noteId in notesMeta) {
+            if (notesMeta.hasOwnProperty(noteId)) {
+                this._notesCache.getNoteInfo(notesMeta[noteId], Y.bind(this._addNoteInfo, this));
+            }
         }
     },
 
@@ -63,32 +77,14 @@ var NotesManager = Y.Base.create('notesManager', Y.Base, [], {
         this._notesInfoList.add(noteInfo);
         this._notesProcessed++;
 
-        this.fire('noteRead', {
-            note : noteInfo,
-            cached : false
-        });
-
         if (this._notesProcessed == this._notesLength) {
             this._internalState = State.LOADED;
-            this.fire('allNotesRead');
-
             this._notesCache.persist();
         }
     },
 
     getNotesModel : function() {
         return this._notesInfoList;
-    },
-
-    getNote : function(id, callback) {
-        this._notesCache.getNoteInfo({
-            id : id,
-            revision : null
-        }, Y.bind(this._returnNote, this, callback));
-    },
-
-    _returnNote : function(callback, noteInfo) {
-        this._notesCache.getNote(noteInfo, callback);
     },
 
     getState : function() {
